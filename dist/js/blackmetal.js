a22aab7779128d5fb63d2d7ae3f9ee4203f7ae6a
Jeffrey777A <44594550+jeffrey-agyepong@users.noreply.github.com>
2021-03-18 02:38:02 +0000

    .

---
+++ dist/js/blackmetal.js
@@ -0,0 +1,22 @@
+var bandData = [{
+  name: "Bismoth",
+  photo: "dist/img/bismoth.webp",
+},
+
+
+
+]
+
+new Vue({
+  el:'#vue-app',
+  data:{
+       cardinfos:bandData,
+       currentIndx: 0
+
+   },
+
+   methods: {}
+
+  
+
+});
