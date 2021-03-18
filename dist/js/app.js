02713aed4b7b7562164594067edb9c341e30fcfe
Jeffrey777A <44594550+jeffrey-agyepong@users.noreply.github.com>
2021-03-18 03:36:08 +0000

    .

---
+++ dist/js/app.js
@@ -0,0 +1,77 @@
+var bandData = [
+
+  {
+    name: "Afterimage",
+    photo: "dist/img/deathmetal/afterimage.webp",
+    link:"beyondthegravemusic.com"
+  },
+  {
+    name: "Arthure",
+    photo: "dist/img/deathmetal/arthure.webp"
+  },
+  {
+    name: "Bismoth",
+    photo: "dist/img/bismoth.webp",
+    
+  },
+  {
+    name: "Astillian",
+    photo: "dist/img/deathmetal/astillian.webp"
+  },
+  {
+    name: "Cruentis",
+    photo: "dist/img/deathmetal/cruentis.webp"
+  },
+  {
+    name: "Diatheke",
+    photo: "dist/img/deathmetal/diatheke.webp"
+  },
+  {
+    name:"Elephant Watchtower",
+    photo:"dist/img/deathmetal/elephant-watchtower.webp"
+  },
+  {
+   name:"Empty Grave",
+   photo:"dist/img/deathmetal/emptygrave.webp" 
+  },
+  {
+  name: "Every Knee Shall Bow",
+  photo:"dist/img/deathmetal/eksb.webp" 
+  },
+  {
+    name: "Hashem",
+    photo: "dist/img/deathmetal/hashem.webp"
+  },
+  {
+    name:"Incarnate Deity",
+    photo: "dist/img/deathmetal/incarnatedeity.webp"
+  },
+  {
+    name:"Roots of Tragedy",
+    photo: "dist/img/deathmetal/roots-of-tragedy.webp"
+  },
+
+{
+  name: "His Kingdom Suffers",
+  photo: "dist/img/deathcore/hks.webp",
+},
+{
+  name: "Searching Serenity",
+  photo: "dist/img/deathcore/searchingserenity.webp"
+},
+
+
+]
+
+new Vue({
+  el:'#vue-app',
+  data:{
+       cardinfos:bandData,
+       currentIndx: 0
+   },
+
+   methods: {}
+
+  
+
+});
