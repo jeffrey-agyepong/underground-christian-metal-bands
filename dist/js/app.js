7a375b964ed1b730b3ec46f18f1a41227e0e265b
Jeffrey777A <44594550+jeffrey-agyepong@users.noreply.github.com>
2021-03-18 06:22:50 +0000

    .

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,93 +1,8 @@
-/*var bandData = [
-
-  {
-    name: "Afterimage",
-    photo: "dist/img/deathmetal/afterimage.webp",
-    link:"beyondthegravemusic.com"
-  },
-  {
-    name: "Arthure",
-    photo: "dist/img/deathmetal/arthure.webp"
-  },
-  {
-    name: "Bismoth",
-    photo: "dist/img/bismoth.webp",
-    
-  },
-  {
-    name: "Astillian",
-    photo: "dist/img/deathmetal/astillian.webp"
-  },
-  {
-    name: "Cruentis",
-    photo: "dist/img/deathmetal/cruentis.webp"
-  },
-  {
-    name: "Diatheke",
-    photo: "dist/img/deathmetal/diatheke.webp"
-  },
-  {
-    name:"Elephant Watchtower",
-    photo:"dist/img/deathmetal/elephant-watchtower.webp"
-  },
-  {
-   name:"Empty Grave",
-   photo:"dist/img/deathmetal/emptygrave.webp" 
-  },
-  {
-  name: "Every Knee Shall Bow",
-  photo:"dist/img/deathmetal/eksb.webp" 
-  },
-  {
-    name: "Hashem",
-    photo: "dist/img/deathmetal/hashem.webp"
-  },
-  {
-    name:"Incarnate Deity",
-    photo: "dist/img/deathmetal/incarnatedeity.webp"
-  },
-  {
-    name:"Roots of Tragedy",
-    photo: "dist/img/deathmetal/roots-of-tragedy.webp"
-  },
-
-{
-  name: "His Kingdom Suffers",
-  photo: "dist/img/deathcore/hks.webp",
-},
-{
-  name: "Searching Serenity",
-  photo: "dist/img/deathcore/searchingserenity.webp"
-},
-
-
-]
-
-new Vue({
-  el:'#vue-app',
-  data:{
-       cardinfos:bandData,
-       currentIndx: 0
-   },
-   links:[
-
-    {link:"./bands/aftermage.html"},
-    {link:"./bands/aftermage.html"}
-   ],
-
-   methods: {}
-
-  
-
-});*/
-
-
 
 class Project {
-  constructor(title, link, author, img, category) {
+  constructor(title, link, img, category) {
       this.title = title,
-      this.link = link,
-      this.author = author, 
+      this.link = link, 
       this.img = img,
       this.category = category
   }
@@ -101,43 +16,37 @@ data: {
           new Project (
               'Afterimage', 
               './Bands/afterimage.html', 
-              'Dessert Tools za', 
               'dist/img/deathmetal/afterimage.webp',
               'DESIGN'
         ),
           new Project (
               'Astillian', 
               'https://digitalassetmanagement.com/', 
-              'Widen',
               'dist/img/deathmetal/astillian.webp',
               'DESIGN'
           ),
           new Project (
               'Arthure', 
-              'https://meet-a-geek.today/', 
-              'Contenforces', 
+              'https://meet-a-geek.today/',  
               'dist/img/deathmetal/arthure.webp',
               'DEVELOPMENT'
           ),
           new Project (
               'Cruentis', 
               'https://www.smarttraining.com/', 
-              'Smart Training LCC',
               'dist/img/deathmetal/cruentis.webp',
               'DEVELOPMENT'
           ),
           new Project (
               'Diathekeke', 
-              'https://micencostagebank.nl/index_september.php', 
-              'Contenforces', 
+              'https://micencostagebank.nl/index_september.php',  
               'dist/img/deathmetal/diatheke.webp',
               'COLABORATION'
           ),
           
           new Project (
               'Elephant Watchtower', 
-              'http://prettyexcellent.com/applecider/', 
-              'A. A. Ron',
+              'http://prettyexcellent.com/applecider/',
               'dist/img/deathmetal/elephant-watchtower.webp',
               'DEVELOPMENT'
           )
--- dist/js/deathmetal.js
+++ dist/js/deathmetal.js
@@ -1,62 +1,62 @@
-var bandData = [{
-  name: "Afterimage",
-  photo: "dist/img/deathmetal/afterimage.webp",
-},
-{
-  name: "Arthure",
-  photo: "dist/img/deathmetal/arthure.webp"
-},
-{
-  name: "Astillian",
-  photo: "dist/img/deathmetal/astillian.webp"
-},
-{
-  name: "Cruentis",
-  photo: "dist/img/deathmetal/cruentis.webp"
-},
-{
-  name: "Diatheke",
-  photo: "dist/img/deathmetal/diatheke.webp"
-},
-{
-  name:"Elephant Watchtower",
-  photo:"dist/img/deathmetal/elephant-watchtower.webp"
-},
-{
- name:"Empty Grave",
- photo:"dist/img/deathmetal/emptygrave.webp" 
-},
-{
-name: "Every Knee Shall Bow",
-photo:"dist/img/deathmetal/eksb.webp" 
-},
-{
-  name: "Hashem",
-  photo: "dist/img/deathmetal/hashem.webp"
-},
-{
-  name:"Incarnate Deity",
-  photo: "dist/img/deathmetal/incarnatedeity.webp"
-},
-{
-  name:"Roots of Tragedy",
-  photo: "dist/img/deathmetal/roots-of-tragedy.webp"
-},
 
-
-
-]
+class Project {
+  constructor(title, link, img, category) {
+      this.title = title,
+      this.link = link, 
+      this.img = img,
+      this.category = category
+  }
+}
 
 new Vue({
-  el:'#vue-app',
-  data:{
-       cardinfos:bandData,
-       currentIndx: 0
+el: '#vue-app',
+data: {
+      currentFilter: 'ALL',
+      projectList: [
+          new Project (
+              'Afterimage', 
+              './Bands/afterimage.html', 
+              'dist/img/deathmetal/afterimage.webp',
+              ''
+        ),
+          new Project (
+              'Astillian', 
+              'https://digitalassetmanagement.com/', 
+              'dist/img/deathmetal/astillian.webp',
+              ''
+          ),
+          new Project (
+              'Arthure', 
+              'https://meet-a-geek.today/',  
+              'dist/img/deathmetal/arthure.webp',
+              ''
+          ),
+          new Project (
+              'Cruentis', 
+              'https://www.smarttraining.com/', 
+              'dist/img/deathmetal/cruentis.webp',
+              ''
+          ),
+          new Project (
+              'Diathekeke', 
+              'https://micencostagebank.nl/index_september.php',  
+              'dist/img/deathmetal/diatheke.webp',
+              ''
+          ),
+          
+          new Project (
+              'Elephant Watchtower', 
+              '',
+              'dist/img/deathmetal/elephant-watchtower.webp',
+              ''
+          )
+      ], 
+  },
 
-   },
-
-   methods: {}
-
+methods: {
+  setFilter: function setFilter(filter) {
+    this.currentFilter = filter;
+      } 
+  }
   
-
 });
