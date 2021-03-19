7b2062f0e390fe0126564a55dc55286ff77a6201
Jeffrey777A <44594550+jeffrey-agyepong@users.noreply.github.com>
2021-03-19 00:35:47 +0000

    .

--- dist/js/app.js
+++ dist/js/app.js
@@ -14,9 +14,9 @@ data: {
       currentFilter: 'ALL',
       projectList: [
         new Project (
-            'Aeonon', 
-            './Bands/afterimage.html', 
-            'dist/img/metalcore/aeonon.webp',
+            'Aenon', 
+            './Bands/aenon.html', 
+            'dist/img/metalcore/aenon.webp',
             'DESIGN'
       ),
           new Project (
@@ -33,85 +33,85 @@ data: {
           ),
           new Project (
               'Arthure', 
-              'https://meet-a-geek.today/',  
+              './Bands/afterimage.html',  
               'dist/img/deathmetal/arthure.webp',
               'DEVELOPMENT'
           ),
           new Project (
             'Ben Dixon', 
-            'https://www.smarttraining.com/', 
+            './Bands/afterimage.html', 
             'dist/img/metalcore/ben-dixon.webp',
             'DEVELOPMENT'
         ),
         new Project (
             'Bloodlines', 
-            'https://www.smarttraining.com/', 
+            './Bands/afterimage.html', 
             'dist/img/metalcore/bloodlines.webp',
             'DEVELOPMENT'
         ),
         new Project (
             'Blue Fire Horizon', 
-            'https://www.smarttraining.com/', 
+            './Bands/afterimage.html', 
             'dist/img/metalcore/blue-fire-horizon.webp',
             'DEVELOPMENT'
         ),
         new Project (
             'Collapse//Revive', 
-            'https://www.smarttraining.com/', 
+            './Bands/afterimage.html', 
             'dist/img/metalcore/collapse-revive.webp',
             'DEVELOPMENT'
         ),
         new Project (
             'Context', 
-            'https://www.smarttraining.com/', 
+            './Bands/afterimage.html', 
             'dist/img/metalcore/cntxt.webp',
             'DEVELOPMENT'
         ),
         new Project (
             'Crowned in Sorrow', 
-            'https://www.smarttraining.com/', 
+            './Bands/afterimage.html', 
             'dist/img/deathmetal/derek-corzine.webp',
             'DEVELOPMENT'
         ),
         new Project (
             'Cruentis', 
-            'https://www.smarttraining.com/', 
+            './Bands/afterimage.html', 
             'dist/img/deathmetal/cruentis.webp',
             'DEVELOPMENT'
         ),
         new Project (
             'Diathekeke', 
-            'https://micencostagebank.nl/index_september.php',  
+            './Bands/afterimage.html',  
             'dist/img/deathmetal/diatheke.webp',
             'COLABORATION'
         ),
         new Project (
             'Elephant Watchtower', 
-            'http://prettyexcellent.com/applecider/',
+            './Bands/afterimage.html',
             'dist/img/deathmetal/elephant-watchtower.webp',
             'DEVELOPMENT'
         ),
         new Project (
             'Empty Grave', 
-            'https://www.smarttraining.com/', 
+            './Bands/afterimage.html', 
             'dist/img/deathmetal/emptygrave.webp',
             'DEVELOPMENT'
         ),
         new Project (
             'Eonia', 
-            'https://www.smarttraining.com/', 
+            './Bands/afterimage.html', 
             'dist/img/metalcore/eonia.webp',
             'DEVELOPMENT'
         ),
         new Project (
             'Every Knee Shall Bow', 
-            'https://www.smarttraining.com/', 
+            './Bands/afterimage.html', 
             'dist/img/deathmetal/eksb.webp',
             'DEVELOPMENT'
         ),
           new Project (
               'Incarnate Deity', 
-              'https://www.smarttraining.com/', 
+              './Bands/afterimage.html', 
               'dist/img/deathmetal/incarnatedeity.webp',
               'DEVELOPMENT'
           ),
