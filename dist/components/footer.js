5644e1caef5b2d6836b0f25b5b298fa4ee69e55b
Jeffrey777A <44594550+jeffrey-agyepong@users.noreply.github.com>
2021-03-23 04:46:15 +0000

    .

--- dist/components/footer.js
+++ dist/components/footer.js
@@ -10,8 +10,8 @@ this.innerHTML =`
 <div class="container">
   <div class="row">
     <div class="col l6 s12">
-      <h5 class="white-text">Consider Supporting Us</h5>
-      <p class="grey-text text-lighten-4"> <a href="https://www.buymeacoffee.com/beyondthegrave" target="_blank"><img
+      <h5 class="white-text center">Consider Supporting Us</h5>
+      <p class="grey-text text-lighten-4 center"> <a href="https://www.buymeacoffee.com/beyondthegrave" target="_blank"><img
       src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee"
       style="height: 60px !important;width: 217px !important;"></a></p>
     </div>
@@ -19,18 +19,18 @@ this.innerHTML =`
     <h5 class="white-text center-align">Connect With Us</h5>
     <div class="flex icons center-align">
 
-      <span><a href="https://web.facebook.com/Beyondthegrave777" class="icon brands fab fa-facebook fa-3x"><span
+      <span class="span"><a href="https://web.facebook.com/Beyondthegrave777" class="icon brands fab fa-facebook fa-3x"><span
             class="label"></span></a></span>
-      <span><a href="https://www.instagram.com/beyond.the.grave_/" class="icon brands fab fa-instagram fa-3x"><span
+      <span class="span"><a href="https://www.instagram.com/beyond.the.grave_/" class="icon brands fab fa-instagram fa-3x"><span
             class="label"></span></a></span>
-      <span><a href="https://www.youtube.com/channel/UCCXafO-fbIZnTHNoRlx2QWg
+      <span class="span"><a href="https://www.youtube.com/channel/UCCXafO-fbIZnTHNoRlx2QWg
         " class="icon brands fab fa-youtube fa-3x"><span class="label"></span></a></span>
 
     </div>
     </div>
   </div>
 </div>
-<div class="footer-copyright">
+<div class="footer-copyright left-align">
   <div class="container">
   © 2021 Beyond The Grave
   <a class="grey-text text-lighten-4 right" href="#!">Concise Web Designs</a>
