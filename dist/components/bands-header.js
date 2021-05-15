fb2e11a32f467244e3d9d2cdce8ed223156751d9
Jeffrey <agyepong99@gmail.com>
2021-05-15 06:06:49 +0000

    store

--- dist/components/bands-header.js
+++ dist/components/bands-header.js
@@ -13,27 +13,25 @@ this.innerHTML = `
         <a href="https://beyondthegravemusic.com">BEYOND THE GRAVE</a>
 
         <ul class="right hide-on-med-and-down">
-          <li> <a href="index.html">Bands</a></li>
-          <li><a href="https://www.beyondthegravemusic.com/tag/Interviews/">Interviews</a></li>
-          <li><a href="https://www.beyondthegravemusic.com/tag/Reviews/">Reviews</a></li>
-          <li><a href="https://www.beyondthegravemusic.com/tag/News/">News</a></li>
-          <li><a href="https://www.beyondthegravemusic.com/submit/">Submit</a></li>
-          <li><a href="https://www.beyondthegravemusic.com/about/">About</a></li>
-          <li><a href="https://www.hire.beyondthegravemusic.com" >Hire</a></li>
-         
-
+        <li> <a href="index.html">Bands</a></li>
+        <li><a href="https://www.beyondthegravemusic.com/tag/Interviews/">Interviews</a></li>
+        <li><a href="https://www.beyondthegravemusic.com/tag/Reviews/">Reviews</a></li>
+        <li><a href="https://www.beyondthegravemusic.com/tag/News/">News</a></li>
+        <li><a href="https://store.beyondthegravemusic.com">Store</a></li>
+        <li><a href="https://www.beyondthegravemusic.com/submit/">Submit</a></li>
+        <li><a href="https://www.beyondthegravemusic.com/about/">About</a></li>
+        <li><a href="https://www.hire.beyondthegravemusic.com">Hire</a></li>
         </ul>
 
         <ul id="nav-mobile" class="sidenav black center">
-          
-          <li> <a href="index.html">Bands</a></li>
-          <li><a href="index.html" class="white-text">Bands</a></li>
-          <li><a href="https://www.beyondthegravemusic.com/tag/Interviews/" class="white-text">Interviews</a></li>
-          <li><a href="https://www.beyondthegravemusic.com/tag/Reviews/" class="white-text">Reviews</a></li>
-          <li><a href="https://www.beyondthegravemusic.com/tag/News/" class="white-text">News</a></li>    
-          <li><a href="https://www.beyondthegravemusic.com/submit/" class="white-text">Submit</a></li>
-          <li><a href="https://www.beyondthegravemusic.com/about/" class="white-text">About</a></li>
-          <li><a href="https://www.hire.beyondthegravemusic.com" class="white-text">Hire</a></li>
+        <li><a href="index.html" class="white-text">Bands</a></li>
+        <li><a href="https://www.beyondthegravemusic.com/tag/Interviews/" class="white-text">Interviews</a></li>
+        <li><a href="https://www.beyondthegravemusic.com/tag/Reviews/" class="white-text">Reviews</a></li>
+        <li><a href="https://www.beyondthegravemusic.com/tag/News/" class="white-text">News</a></li>
+        <li><a href="https://store.beyondthegravemusic.com" class="white-text">Store</a></li>
+        <li><a href="https://www.beyondthegravemusic.com/submit/" class="white-text">Submit</a></li>
+        <li><a href="https://www.beyondthegravemusic.com/about/" class="white-text">About</a></li>
+        <li><a href="https://www.hire.beyondthegravemusic.com" class="white-text">Hire</a></li>
         </ul>
         <a href="#" data-target="nav-mobile" class="sidenav-trigger header_toggle">
           <span></span>
--- dist/components/header.js
+++ dist/components/header.js
@@ -17,10 +17,11 @@ this.innerHTML = `
           <li><a href="https://www.beyondthegravemusic.com/tag/Interviews/">Interviews</a></li>
           <li><a href="https://www.beyondthegravemusic.com/tag/Reviews/">Reviews</a></li>
           <li><a href="https://www.beyondthegravemusic.com/tag/News/">News</a></li>
+          <li><a href="https://store.beyondthegravemusic.com">Store</a></li>
           <li><a href="https://www.beyondthegravemusic.com/submit/">Submit</a></li>
           <li><a href="https://www.beyondthegravemusic.com/about/">About</a></li>
           <li><a href="https://www.hire.beyondthegravemusic.com">Hire</a></li>
-         
+
 
         </ul>
 
@@ -28,7 +29,8 @@ this.innerHTML = `
           <li><a href="index.html" class="white-text">Bands</a></li>
           <li><a href="https://www.beyondthegravemusic.com/tag/Interviews/" class="white-text">Interviews</a></li>
           <li><a href="https://www.beyondthegravemusic.com/tag/Reviews/" class="white-text">Reviews</a></li>
-          <li><a href="https://www.beyondthegravemusic.com/tag/News/" class="white-text">News</a></li>    
+          <li><a href="https://www.beyondthegravemusic.com/tag/News/" class="white-text">News</a></li>
+          <li><a href="https://store.beyondthegravemusic.com" class="white-text">Store</a></li>    
           <li><a href="https://www.beyondthegravemusic.com/submit/" class="white-text">Submit</a></li>
           <li><a href="https://www.beyondthegravemusic.com/about/" class="white-text">About</a></li>
           <li><a href="https://www.hire.beyondthegravemusic.com" class="white-text">Hire</a></li>
