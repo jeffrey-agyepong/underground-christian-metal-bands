e4fb98d8985d57f63162703299edc12106d78095
Jeffrey <agyepong99@gmail.com>
2021-06-03 03:34:57 +0000

    .

--- dist/components/bands-header.js
+++ dist/components/bands-header.js
@@ -17,8 +17,8 @@ class Header extends HTMLElement {
         <li><a href="https://www.beyondthegravemusic.com/tag/Reviews/">Reviews</a></li>
         <li><a href="https://www.beyondthegravemusic.com/tag/News/">News</a></li>
           <!--<li><a href="https://store.beyondthegravemusic.com">Store</a></li>-->
-        <li><a href="https://www.beyondthegravemusic.com/submit/">Submit</a></li>
-        <li><a href="https://bands.beyondthegravemusic.com/support.html">Support</a></li>
+        <li><a href="https://contact.beyondthegravemusic.com/">Contact</a></li>
+        <li><a href="https://support.beyondthegravemusic.com/">Support</a></li>
         <li><a href="https://www.hire.beyondthegravemusic.com">Hire</a></li>
         </ul>
 
@@ -28,8 +28,8 @@ class Header extends HTMLElement {
         <li><a href="https://www.beyondthegravemusic.com/tag/Reviews/" class="white-text">Reviews</a></li>
         <li><a href="https://www.beyondthegravemusic.com/tag/News/" class="white-text">News</a></li>
       <!--<li><a href="https://store.beyondthegravemusic.com" class="white-text">Store</a></li>-->
-        <li><a href="https://www.beyondthegravemusic.com/submit/" class="white-text">Submit</a></li>
-        <li><a href="https://bands.beyondthegravemusic.com/support.html" class="white-text">Support</a></li>
+        <li><a href="https://contact.beyondthegravemusic.com/" class="white-text">Contact</a></li>
+        <li><a href="https://support.beyondthegravemusic.com/" class="white-text">Support</a></li>
         <li><a href="https://www.hire.beyondthegravemusic.com" class="white-text">Hire</a></li>
         </ul>
         <a href="#" data-target="nav-mobile" class="sidenav-trigger header_toggle">
