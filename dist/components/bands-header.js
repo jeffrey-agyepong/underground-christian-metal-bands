52334418ccbe2a4823898d72d7ee6e2aa9440ff2
jeffrey <agyepong99@gmail.com>
2021-08-04 00:07:41 +0000

    .

--- dist/components/bands-header.js
+++ dist/components/bands-header.js
@@ -15,22 +15,24 @@ class Header extends HTMLElement {
         <li> <a href="index.html">Bands</a></li>
         <li><a href="https://www.beyondthegravemusic.com/tag/Interviews/">Interviews</a></li>
         <li><a href="https://www.beyondthegravemusic.com/tag/Reviews/">Reviews</a></li>
+        <li><a href="https://www.samplers.beyondthegravemusic.com/">Samplers</a></li>
         <li><a href="https://www.beyondthegravemusic.com/tag/News/">News</a></li>
           <!--<li><a href="https://store.beyondthegravemusic.com">Store</a></li>-->
         <li><a href="https://contact.beyondthegravemusic.com/">Contact</a></li>
         <li><a href="https://support.beyondthegravemusic.com/">Support</a></li>
-        <li><a href="https://www.hire.beyondthegravemusic.com">Hire</a></li>
+
         </ul>
 
         <ul id="nav-mobile" class="sidenav black center">
         <li><a href="index.html" class="white-text">Bands</a></li>
         <li><a href="https://www.beyondthegravemusic.com/tag/Interviews/" class="white-text">Interviews</a></li>
         <li><a href="https://www.beyondthegravemusic.com/tag/Reviews/" class="white-text">Reviews</a></li>
+          <li><a href="https://www.samplers.beyondthegravemusic.com/" class="white-text">Samplers</a></li>
         <li><a href="https://www.beyondthegravemusic.com/tag/News/" class="white-text">News</a></li>
       <!--<li><a href="https://store.beyondthegravemusic.com" class="white-text">Store</a></li>-->
         <li><a href="https://contact.beyondthegravemusic.com/" class="white-text">Contact</a></li>
         <li><a href="https://support.beyondthegravemusic.com/" class="white-text">Support</a></li>
-        <li><a href="https://www.hire.beyondthegravemusic.com" class="white-text">Hire</a></li>
+
         </ul>
         <a href="#" data-target="nav-mobile" class="sidenav-trigger header_toggle">
           <span></span>
